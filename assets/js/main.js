b393bd41f019d2691394bcf5696bc8f4cf26e137
Luis Muñoz <luis.albertto@gmail.com>
2022-04-30 23:03:42 -0400

    primer registro

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.example-popover').popover({
+      container: 'body'
+    })
+  })
+  $(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+    });
+$('#Enviar').oneclick(function() {
+    alert("El formulario fue enviado correctamente");
+    })
+   
